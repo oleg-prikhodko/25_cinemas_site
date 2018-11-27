a5d4c66cb9a0cb061d6c226eba37598683b9cd68
Oleg <super.oleg.3000@yandex.ru>
2018-11-27 11:58:12 +0300

    Add movies api function

--- server.py
+++ server.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import cinemas
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def films_list():
-    return render_template('films_list.html')
+    return render_template("films_list.html")
+
+
+@app.route("/api/movies")
+def get_movies():
+    movies = [
+        {
+            "title": title,
+            "rating_value": rating_value,
+            "rating_count": rating_count,
+        }
+        for title, rating_value, rating_count in cinemas.get_movies()
+    ]
+    return jsonify(movies)
+
 
 if __name__ == "__main__":
     app.run()
